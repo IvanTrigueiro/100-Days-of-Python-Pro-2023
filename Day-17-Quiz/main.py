c69f6648f7b92af8df22025b2d62a61f99df93c7
Ivan <ivantrigueiro-@hotmail.com>
2023-09-19 10:27:04 -0300

    Com2: created quiz_brain and next_question method

--- Day-17-Quiz/main.py
+++ Day-17-Quiz/main.py
@@ -1,9 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
 question_bank = []
 for i in range(0, len(question_data)):
     question_text = question_data[i]['text']
     question_answer = question_data[i]['answer']
     question_bank.append(Question(question_text, question_answer))
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
