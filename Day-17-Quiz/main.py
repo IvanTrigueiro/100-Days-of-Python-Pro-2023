d99babf130fa8919c234b66b74f5ce4880875061
Ivan <ivantrigueiro-@hotmail.com>
2023-09-19 10:50:27 -0300

    Com4: adding quiz data from opentdb.com and adapting code to it

--- Day-17-Quiz/main.py
+++ Day-17-Quiz/main.py
@@ -4,10 +4,13 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for i in range(0, len(question_data)):
-    question_text = question_data[i]['text']
-    question_answer = question_data[i]['answer']
+    question_text = question_data[i]['question']
+    question_answer = question_data[i]['correct_answer']
     question_bank.append(Question(question_text, question_answer))
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
