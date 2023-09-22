7fcb63e1bae1ef0d9d4bfea2570770eaf35be27c
Ivan <ivantrigueiro-@hotmail.com>
2023-09-22 10:36:03 -0300

    Com4: Control snake with arrows and wasd

--- Day-20-Day-21-Snake-game/main.py
+++ Day-20-Day-21-Snake-game/main.py
@@ -10,11 +10,20 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "W")
+screen.onkey(snake.down, "S")
+screen.onkey(snake.left, "A")
+screen.onkey(snake.right, "D")
+
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-
+    time.sleep(0.08)
     snake.move()
 
 screen.exitonclick()
--- Day-20-Day-21-Snake-game/snake.py
+++ Day-20-Day-21-Snake-game/snake.py
@@ -1,12 +1,18 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+LEFT = 180
+RIGHT = 0
+DOWN = 270
 
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.snake_head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -21,4 +27,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.snake_head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
