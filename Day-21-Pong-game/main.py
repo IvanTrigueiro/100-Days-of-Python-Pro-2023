d17e17e7f042d05ca83fc8d51fdb35dfb4c9e814
Ivan <ivantrigueiro-@hotmail.com>
2023-09-24 09:35:47 -0300

    Com1: Pong game first steps

---
+++ Day-21-Pong-game/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+HEIGHT = 600
+WIDTH = 800
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.title("Pong")
+
+screen.exitonclick()
+
