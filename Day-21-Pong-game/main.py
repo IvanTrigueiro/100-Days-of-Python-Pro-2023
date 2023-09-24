3688da3ca065a8bf62c79ef2607fe50e097d765b
Ivan <ivantrigueiro-@hotmail.com>
2023-09-24 11:30:51 -0300

    Com3: Finished Pong Game

--- Day-21-Pong-game/main.py
+++ Day-21-Pong-game/main.py
@@ -1,5 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 HEIGHT = 600
 WIDTH = 800
@@ -11,15 +14,33 @@ screen.tracer(0)
 
 paddle_right = Paddle((350, 0))
 paddle_left = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle_right.move_up, "Up")
-screen.onkey(paddle_right.move_down, "Down")
-screen.onkey(paddle_left.move_up, "w")
-screen.onkey(paddle_left.move_down, "s")
+screen.onkeypress(paddle_right.move_up, "Up")
+screen.onkeypress(paddle_right.move_down, "Down")
+screen.onkeypress(paddle_left.move_up, "w")
+screen.onkeypress(paddle_left.move_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 430:
+        ball.reset_position()
+        scoreboard.left_point()
+
+    elif ball.xcor() < -430:
+        ball.reset_position()
+        scoreboard.right_point()
 
 screen.exitonclick()
