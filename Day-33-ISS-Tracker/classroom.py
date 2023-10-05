fd079a7037af860b7f93063a82e6ff662373bd2c
Ivan <ivantrigueiro-@hotmail.com>
2023-10-05 10:13:36 -0300

    Starting challenge

---
+++ Day-33-ISS-Tracker/classroom.py
@@ -0,0 +1,35 @@
+import requests
+import datetime as dt
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(data)
+
+MY_LAT = -7.159768
+MY_LONG = -34.794959
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = dt.datetime.now()
+
+print(sunrise)
+print(sunset)
+print(time_now.hour)
+
