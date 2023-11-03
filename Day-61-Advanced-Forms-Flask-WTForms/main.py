9b37145ab84e79ee1ece21d7ee71d641a255fb9a
Ivan <ivantrigueiro-@hotmail.com>
2023-11-03 10:13:01 -0300

    Day61: Finished project, added FlaskForm for easier form creation.

--- Day-61-Advanced-Forms-Flask-WTForms/main.py
+++ Day-61-Advanced-Forms-Flask-WTForms/main.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-
-app = Flask(__name__)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
+from flask_bootstrap import Bootstrap
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
-    submit = SubmitField("Submit")
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -26,7 +29,7 @@ def login():
             return render_template("success.html")
         else:
             return render_template("denied.html")
-    return render_template('login.html', form=login_form)
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
