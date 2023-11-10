b394f1575815eb71295edcc3afa828dbbcd40efc
Ivan <ivantrigueiro-@hotmail.com>
2023-11-10 09:38:14 -0300

    Day65: Added /all and /search routes.

--- Day-66-Build-a-REST-API/main.py
+++ Day-66-Build-a-REST-API/main.py
@@ -69,6 +69,23 @@ def get_random_cafe():
     return jsonify(cafe=random_cafe.to_dict())
 
 
+@app.route("/all", methods=["GET"])
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafe=[cafe.to_dict() for cafe in cafes])
+
+
+@app.route("/search", methods=["GET"])
+def get_cafe_by_location():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
+
 # HTTP POST - Create Record
 
 # HTTP PUT/PATCH - Update Record
